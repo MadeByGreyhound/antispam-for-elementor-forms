59f3182310bf8286cd16606b7bff0fac69fdbda8
Tom Slominski <tom@slomin.ski>
2023-11-13 22:27:46 +0000

    Remove unnecessary settings fields, add customizable label

--- includes/JS_Honeypot_Field.php
+++ includes/JS_Honeypot_Field.php
@@ -24,6 +24,7 @@ class JS_Honeypot_Field extends \ElementorPro\Modules\Forms\Fields\Field_Base {
         parent::__construct();
 
         add_action( 'elementor_pro/forms/render/item/asef-js-honeypot', [$this, 'container_args'], 10, 3 );
+        add_action( 'elementor/element/form/section_form_fields/before_section_end', [ $this, 'update_controls' ] );
     }
 
     /**
@@ -55,6 +56,7 @@ class JS_Honeypot_Field extends \ElementorPro\Modules\Forms\Fields\Field_Base {
     public function container_args( $item, $item_index, $widget ) {
         $id = 'field-group' . $item_index;
         $widget->set_render_attribute( $id, 'class', 'elementor-field-type-text asef-js-hp-container' );
+        $widget->set_render_attribute( $id, 'data-label', $item['field_label'] ?: __( 'Please leave this field empty', 'antispam-for-elementor-forms' ) );
         $widget->set_render_attribute( $id, 'hidden' );
         $item['field_label'] = false;
 
@@ -74,8 +76,8 @@ class JS_Honeypot_Field extends \ElementorPro\Modules\Forms\Fields\Field_Base {
     /**
      * Field validation.
      *
-     * @param \ElementorPro\Modules\Forms\Classes\Field_Base   $field
-     * @param \ElementorPro\Modules\Forms\Classes\Form_Record  $record
+     * @param \ElementorPro\Modules\Forms\Fields\Field_Base $field
+     * @param \ElementorPro\Modules\Forms\Classes\Form_Record $record
      * @param \ElementorPro\Modules\Forms\Classes\Ajax_Handler $ajax_handler
      * @return void
      */
@@ -86,6 +88,36 @@ class JS_Honeypot_Field extends \ElementorPro\Modules\Forms\Fields\Field_Base {
             // If success - remove the field form list (don't send it in emails etc. )
             $record->remove_field( $field['id'] );
         }
+    }
+
+    /**
+     * Hide surplus field settings.
+     *
+     * @param \Elementor\Widget_Base $widget
+     * @return void
+     */
+    public function update_controls( \Elementor\Widget_Base $widget ) {
+        $elementor = \ElementorPro\Plugin::elementor();
+
+        $control_data = $elementor->controls_manager->get_control_from_stack( $widget->get_unique_name(), 'form_fields' );
+
+        if( is_wp_error( $control_data ) ) {
+            return;
+        }
+
+        foreach( $control_data['fields'] as $index => $field ) {
+            if( 'required' === $field['name'] || 'width' === $field['name'] ) {
+                $control_data['fields'][ $index ]['conditions']['terms'][] = [
+                    'name' => 'field_type',
+                    'operator' => '!in',
+                    'value' => [
+                        'asef-js-honeypot',
+                    ],
+                ];
+            }
+        }
+
+        $widget->update_control( 'form_fields', $control_data );
     }
 
 }
--- src/js/antispam-for-elementor-forms.js
+++ src/js/antispam-for-elementor-forms.js
@@ -4,7 +4,9 @@ document.querySelectorAll( '.elementor-form' ).forEach( form => {
     fields.forEach( field => {
         field.addEventListener( 'focus', () => {
             if( 'true' !== form.dataset.hpFieldSet ) {
-                form.querySelector( '.asef-js-hp-container' ).innerHTML = '<label for="asef-js-hp">Leave this field blank.</label><input type="hidden" name="asef-js-hp" id="asef-js-hp">';
+				const honeypot = form.querySelector( '.asef-js-hp-container' );
+				
+                honeypot.innerHTML = '<label for="asef-js-hp">' + honeypot.dataset.label + '</label><input type="hidden" name="asef-js-hp" id="asef-js-hp">';
                 form.dataset.hpFieldSet = 'true';
             }
         } );
